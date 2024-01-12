03d32d105e113044340d901a24bd7c184af25460
Emin Ulusoy <ulusoyeminn@gmail.com>
2024-01-12 16:03:53 +0300

    template engines is used and page transactions are made

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ import express from "express";
 const app = express()
 const port = 3000
 
+//*ejs yi template engine olarak tanımlamak için aşağıya bunu yazdık
+app.set("view engine", "ejs")
+
+app.use(express.static('public'))
+
 app.get("/", (req,res)=>{
-    res.send("INDEX sayfası 2")
+    res.render("index")
+})
+
+app.get("/about", (req,res)=>{
+    res.render("about")
 })
 
 app.listen(port, ()=> {
